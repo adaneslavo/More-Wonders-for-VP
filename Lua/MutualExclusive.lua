3c107801f3ca5e744f53004f334b42fdf6b8d8ad
adan_eslavo <adan_eslavo@o2.pl>
2020-01-12 12:38:57 +0100

    v0.3.10.2
    Fixes.

--- Lua/MutualExclusive.lua
+++ Lua/MutualExclusive.lua
@@ -1,4 +1,4 @@
-print("Loading MutualExclusive.lua from MWfVP");
+print("Loading MutualExclusive.lua from MWfVP")
 --------------------------------------------------------------
 -- Mutually Exclusive groups of Wonders using CityCanConstruct event
 -- Dec 14, 2017: Created, Infixo
@@ -7,65 +7,65 @@ print("Loading MutualExclusive.lua from MWfVP");
 
 -- debug output routine
 function dprint(sStr,p1,p2,p3,p4,p5,p6)
-	local sOutStr = sStr;
-	if p1 ~= nil then sOutStr = sOutStr.." [1] "..tostring(p1); end
-	if p2 ~= nil then sOutStr = sOutStr.." [2] "..tostring(p2); end
-	if p3 ~= nil then sOutStr = sOutStr.." [3] "..tostring(p3); end
-	if p4 ~= nil then sOutStr = sOutStr.." [4] "..tostring(p4); end
-	if p5 ~= nil then sOutStr = sOutStr.." [5] "..tostring(p5); end
-	if p6 ~= nil then sOutStr = sOutStr.." [6] "..tostring(p6); end
-	print(sOutStr);
+	local sOutStr = sStr
+	if p1 ~= nil then sOutStr = sOutStr.." [1] "..tostring(p1) end
+	if p2 ~= nil then sOutStr = sOutStr.." [2] "..tostring(p2) end
+	if p3 ~= nil then sOutStr = sOutStr.." [3] "..tostring(p3) end
+	if p4 ~= nil then sOutStr = sOutStr.." [4] "..tostring(p4) end
+	if p5 ~= nil then sOutStr = sOutStr.." [5] "..tostring(p5) end
+	if p6 ~= nil then sOutStr = sOutStr.." [6] "..tostring(p6) end
+	print(sOutStr)
 end
 
 -- replacement for CountNumBuildings which produced wrong results during tests (idk)
 function CountNumBuildingsLocal(pPlayer, iWonder)
-	local iTotal = 0;
+	local iTotal = 0
 	
 	for city in pPlayer:Cities() do
-		iTotal = iTotal + city:GetNumRealBuilding(iWonder);
+		iTotal = iTotal + city:GetNumRealBuilding(iWonder)
 	end
 	
-	return iTotal;
+	return iTotal
 end
 
-local tValidWonders = {};
-local tWonderGroups = {};
+local tValidWonders = {}
+local tWonderGroups = {}
 
 -- main function, will be called MANY times, so make it fast!
 function CheckMutualExclusiveness(iPlayer, iCity, iBuildingType)
 	-- if this is NOT a tracked Wonder then quit immediately
-	if not tValidWonders[iBuildingType] then return true; end
+	if not tValidWonders[iBuildingType] then return true end
 	
-	local pPlayer = Players[iPlayer];
+	local pPlayer = Players[iPlayer]
 	
-	if not pPlayer:IsAlive() then return false; end
+	if not pPlayer:IsAlive() then return false end
 	
 	-- check if one of the group is built
-	local iGroup = tWonderGroups[iBuildingType];
+	local iGroup = tWonderGroups[iBuildingType]
 	
 	for wonder, group in pairs(tWonderGroups) do
 		if group == iGroup and CountNumBuildingsLocal(pPlayer, wonder) > 0 then
-			return false;
+			return false
 		end
 	end
 	
-	return true;
+	return true
 end
-GameEvents.CityCanConstruct.Add(CheckMutualExclusiveness);
+GameEvents.CityCanConstruct.Add(CheckMutualExclusiveness)
 
 
 function Initialize()
 	-- find all valid Wonders
 	for building in GameInfo.Buildings() do	
-		local thisBuildingClass = GameInfo.BuildingClasses[building.BuildingClass];
+		local thisBuildingClass = GameInfo.BuildingClasses[building.BuildingClass]
 		
 		if thisBuildingClass.MaxGlobalInstances == 1 and building.IsCorporation == 0 and building.MutuallyExclusiveGroup > 0 then -- exclude Corporations
-			dprint("...adding (id,wonder,group)", building.ID, building.Type, building.MutuallyExclusiveGroup);
-			tValidWonders[building.ID] = true;
-			tWonderGroups[building.ID] = building.MutuallyExclusiveGroup;
+			dprint("...adding (id,wonder,group)", building.ID, building.Type, building.MutuallyExclusiveGroup)
+			tValidWonders[building.ID] = true
+			tWonderGroups[building.ID] = building.MutuallyExclusiveGroup
 		end
 	end
 end
-Initialize();
+Initialize()
 
-print("Loaded MutualExclusive.lua from MWfVP");
+print("Loaded MutualExclusive.lua from MWfVP")
--- Lua/UniqueWonderRequirements.lua
+++ Lua/UniqueWonderRequirements.lua
@@ -1,4 +1,4 @@
-print("Loading UniqueWonderRequirements.lua from MWfVP");
+print("Loading UniqueWonderRequirements.lua from MWfVP")
 --------------------------------------------------------------
 -- Support for true Lake locations for Wonders and Buildings using CityCanConstruct event
 -- Lake is when: FreshWater = 1, Water = 1, MinAreaSize = 1
@@ -9,60 +9,61 @@ print("Loading UniqueWonderRequirements.lua from MWfVP");
 
 -- debug output routine
 function dprint(sStr,p1,p2,p3,p4,p5,p6)
-	local sOutStr = sStr;
-	if p1 ~= nil then sOutStr = sOutStr.." [1] "..tostring(p1); end
-	if p2 ~= nil then sOutStr = sOutStr.." [2] "..tostring(p2); end
-	if p3 ~= nil then sOutStr = sOutStr.." [3] "..tostring(p3); end
-	if p4 ~= nil then sOutStr = sOutStr.." [4] "..tostring(p4); end
-	if p5 ~= nil then sOutStr = sOutStr.." [5] "..tostring(p5); end
-	if p6 ~= nil then sOutStr = sOutStr.." [6] "..tostring(p6); end
-	print(sOutStr);
+	local sOutStr = sStr
+	if p1 ~= nil then sOutStr = sOutStr.." [1] "..tostring(p1) end
+	if p2 ~= nil then sOutStr = sOutStr.." [2] "..tostring(p2) end
+	if p3 ~= nil then sOutStr = sOutStr.." [3] "..tostring(p3) end
+	if p4 ~= nil then sOutStr = sOutStr.." [4] "..tostring(p4) end
+	if p5 ~= nil then sOutStr = sOutStr.." [5] "..tostring(p5) end
+	if p6 ~= nil then sOutStr = sOutStr.." [6] "..tostring(p6) end
+	print(sOutStr)
 end
 
-local tValidBuildingsLake = {};
-local tValidBuildingsMineBuildings = {};
-local tValidBuildingsMineResources = {};
-local tValidBuildingsWithProhibitedTerrain = {};
-local tProhibitedTerrainForBuilding = {};
+local tValidBuildingsLake = {}
+local tValidBuildingsMineBuildings = {}
+local tValidBuildingsMineResources = {}
+local tValidBuildingsWithProhibitedTerrain = {}
+local tProhibitedTerrainForBuilding = {}
 
--- main function, will be called MANY times, so make it fast!
+-- checks if city is between lake and sea and adds this condition (normally it would be treated like city wothout lake)
 function LakeWithOcean(iPlayer, iCity, iBuildingType)
-	if not tValidBuildingsLake[iBuildingType] then return true; end
+	if not tValidBuildingsLake[iBuildingType] then return true end
    
-	local pPlayer = Players[iPlayer];
+	local pPlayer = Players[iPlayer]
    
-	if not pPlayer:IsAlive() then return false; end
+	if not pPlayer:IsAlive() then return false end
 
-	local pCity = pPlayer:GetCityByID(iCity);
-	local iCityX = pCity:GetX();
-	local iCityY = pCity:GetY();
+	local pCity = pPlayer:GetCityByID(iCity)
+	local iCityX = pCity:GetX()
+	local iCityY = pCity:GetY()
 
 	if pCity:IsCoastal(1) then -- city is adjacent to at least one water tile
 		for dir = 0, DirectionTypes.NUM_DIRECTION_TYPES - 1 do
 			if Map.PlotDirection(iCityX, iCityY, dir):IsLake() then
-				return true;
+				return true
 			end
 		end
 	end
 
-	return false;
+	return false
 end
-GameEvents.CityCanConstruct.Add(LakeWithOcean);
+GameEvents.CityCanConstruct.Add(LakeWithOcean)
 
+-- looks for mine in the city terrain
 function CityWithMine(iPlayer, iCity, iBuildingType)
-	if not tValidBuildingsMineBuildings[iBuildingType] then return true; end
+	if not tValidBuildingsMineBuildings[iBuildingType] then return true end
    
-	local pPlayer = Players[iPlayer];
+	local pPlayer = Players[iPlayer]
    
-	if not pPlayer:IsAlive() then return false; end
+	if not pPlayer:IsAlive() then return false end
 
-	local pCity = pPlayer:GetCityByID(iCity);
-	local iCityX = pCity:GetX();
-	local iCityY = pCity:GetY();
+	local pCity = pPlayer:GetCityByID(iCity)
+	local iCityX = pCity:GetX()
+	local iCityY = pCity:GetY()
 
 	--[[for resource in tValidBuildingsMineResources() do
 		if pCity:IsHasResourceLocal(resource) then
-			return true;
+			return true
 		end
 	end--]]
 
@@ -71,80 +72,83 @@ function CityWithMine(iPlayer, iCity, iBuildingType)
 		local iPlotOwner = pSpecificPlot:GetOwner()
 				
 		if iPlotOwner == iPlayer and pSpecificPlot:GetImprovementType() == GameInfoTypes.IMPROVEMENT_MINE then
-			return true;
+			return true
 		end
 	end
 
-	return false;
+	return false
 end
-GameEvents.CityCanConstruct.Add(CityWithMine);
+GameEvents.CityCanConstruct.Add(CityWithMine)
 
+-- expands ProhibitedCityTerrain function by tiles around the city
+-- adds condition: if not tundra then also not snow
+-- 0 Grass, 1 Plains, 2 Desert, 3 Tundra, 4 Snow, 5 Coast, 6 Ocean, 7 Mountain, 8 Hill
 function ProhibitionAround(iPlayer, iCity, iBuildingType)
-	if not tValidBuildingsWithProhibitedTerrain[iBuildingType] then return true; end
+	if not tValidBuildingsWithProhibitedTerrain[iBuildingType] then return true end
    
-	local pPlayer = Players[iPlayer];
+	local pPlayer = Players[iPlayer]
    
-	if not pPlayer:IsAlive() then return false; end
+	if not pPlayer:IsAlive() then return false end
 
-	local pCity = pPlayer:GetCityByID(iCity);
-	local iCityTerrain = pCity:GetCityIndexPlot(0):GetTerrainType();
-	local iProhibitedTerrain = tProhibitedTerrainForBuilding[iBuildingType];
+	local pCity = pPlayer:GetCityByID(iCity)
+	local iCityTerrain = pCity:GetCityIndexPlot(0):GetTerrainType()
+	local iProhibitedTerrain = tProhibitedTerrainForBuilding[iBuildingType]
 	
-	if iCityTerrain == iProhibitedTerrain then
-		return false;
-	elseif iProhibitedTerrain == 3 and iCityTerrain == GameInfoTypes.TERRAIN_SNOW then
-		return false;
+	--[[if iCityTerrain == iProhibitedTerrain then
+		return false -- already part of existing functionPRohibitedCityTerrain
+	else--]]if iProhibitedTerrain == 3 and iCityTerrain == 4 then
+		return false
 	end
 	
-	local iCityX = pCity:GetX();
-	local iCityY = pCity:GetY();
+	local iCityX = pCity:GetX()
+	local iCityY = pCity:GetY()
 	
 	for dir = 0, DirectionTypes.NUM_DIRECTION_TYPES - 1 do
 		local iTerrain = Map.PlotDirection(iCityX, iCityY, dir):GetTerrainType()
 		
 		if iTerrain == iProhibitedTerrain then
-			return false;
+			return false
 		elseif iProhibitedTerrain == 3 and iTerrain == 4 then
-			return false;
+			return false
 		end
 	end
 	
-	return true;
+	return true
 end
-GameEvents.CityCanConstruct.Add(ProhibitionAround);
+GameEvents.CityCanConstruct.Add(ProhibitionAround)
 
 function Initialize()
 	-- add lake buildings ==> lake is when: FreshWater = 1, Water = 1, MinAreaSize = 1
 	for building in GameInfo.Buildings() do	
 		if building.FreshWater and building.Water and building.MinAreaSize == 1 and building.IsCorporation == 0 then
-			local iBuilding = GameInfoTypes[building.Type];
+			local iBuilding = GameInfoTypes[building.Type]
 			
-			dprint("...adding (id,building,requirement)", building.ID, building.Type, "(Building require lake)");
-			tValidBuildingsLake[building.ID] = true;
+			dprint("...adding (id,building,requirement)", building.ID, building.Type, "(Building require lake)")
+			tValidBuildingsLake[building.ID] = true
 		end
 	end
 
 	-- add mine buildings
-	dprint("...adding (id,building,requirement)", GameInfo.Buildings.BUILDING_TERRACOTTA_ARMY.ID, GameInfo.Buildings.BUILDING_TERRACOTTA_ARMY.Type, "(Building require mine)");
-	tValidBuildingsMineBuildings[GameInfo.Buildings.BUILDING_TERRACOTTA_ARMY.ID] = true;
+	dprint("...adding (id,building,requirement)", GameInfo.Buildings.BUILDING_TERRACOTTA_ARMY.ID, GameInfo.Buildings.BUILDING_TERRACOTTA_ARMY.Type, "(Building require mine)")
+	tValidBuildingsMineBuildings[GameInfo.Buildings.BUILDING_TERRACOTTA_ARMY.ID] = true
 
 	-- add mining resources (currently unused)
 	for improvement in GameInfo.Improvement_ResourceTypes() do
 		if improvement.ImprovementType == "IMPROVEMENT_MINE" then
-			dprint("...adding (id,resource,requirement)", GameInfo.Resources[improvement.ResourceType].ID, improvement.ResourceType, "(Mining Resource)");
-			tValidBuildingsMineResources[GameInfo.Resources[improvement.ResourceType].ID] = true;
+			dprint("...adding (id,resource,requirement)", GameInfo.Resources[improvement.ResourceType].ID, improvement.ResourceType, "(Mining Resource)")
+			tValidBuildingsMineResources[GameInfo.Resources[improvement.ResourceType].ID] = true
 		end
 	end
 
 	-- add buildings with prohibited terrain
 	for building in GameInfo.Buildings() do
 		if building.ProhibitedCityTerrain ~= nil then
-			dprint("...adding (id,building,prohibition,terrain)", building.ID, building.Type, "(Building is prohibited on: " .. building.ProhibitedCityTerrain .. ")");
-			tValidBuildingsWithProhibitedTerrain[building.ID] = true;
-			tProhibitedTerrainForBuilding[building.ID] = GameInfo.Terrains[building.ProhibitedCityTerrain].ID;
+			dprint("...adding (id,building,prohibition,terrain)", building.ID, building.Type, "(Building is prohibited on: " .. building.ProhibitedCityTerrain .. ")")
+			tValidBuildingsWithProhibitedTerrain[building.ID] = true
+			tProhibitedTerrainForBuilding[building.ID] = GameInfo.Terrains[building.ProhibitedCityTerrain].ID
 		end
 	end
 end
-Initialize();
+Initialize()
 
-print("Loaded UniqueWonderRequirements.lua from MWfVP");
+print("Loaded UniqueWonderRequirements.lua from MWfVP")
