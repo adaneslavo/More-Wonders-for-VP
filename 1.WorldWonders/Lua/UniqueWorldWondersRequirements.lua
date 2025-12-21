0acfb5eb52718e0ec25bc72f0e6bd183389b87d5
adan_eslavo <adan_eslavo@o2.pl>
2025-12-21 12:29:48 +0100

    v24.4

--- 1.WorldWonders/Lua/UniqueWorldWondersRequirements.lua
+++ 1.WorldWonders/Lua/UniqueWorldWondersRequirements.lua
@@ -107,8 +107,8 @@ if bIsRestrictionEra then
 							local iCultureReturn = math.ceil(0.33 * iReturnProduction)
 
 							pPlayer:AddNotification(NotificationTypes.NOTIFICATION_WONDER_BEATEN, 
-								'It is too late for building [COLOR_CYAN]' .. sWonderName .. '[ENDCOLOR] Wonder in [COLOR_CYAN]' .. sCityName .. '[ENDCOLOR], because Player has just entered [COLOR_CYAN]' .. sEraName .. '[ENDCOLOR]. Player will receive ' .. iCultureReturn .. '[ICON_CULTURE] Culture in return.', -- use TXT_KEY_ here
-								'[COLOR_CYAN]' .. sWonderName .. '[ENDCOLOR] cannot be built anymore',
+								Locale.ConvertTextKey('TXT_KEY_NOTIFICATION_CULTURE_RETURN_MAX_ERA', sWonderName, sCityName, sEraName, iCultureReturn),
+								Locale.ConvertTextKey('TXT_KEY_NOTIFICATION_CULTURE_RETURN_MAX_ERA_TITLE', sWonderName),
 								pCity:GetX(), pCity:GetY(), building.ID)
 						
 							pPlayer:ChangeJONSCulture(iCultureReturn)
